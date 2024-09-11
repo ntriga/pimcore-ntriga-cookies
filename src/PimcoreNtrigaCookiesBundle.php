d88b65ef3b4fae45383f728d8333ab116a30cf49
Vincent Bibauw <vincent@shimmr.be>
2024-09-11 12:34:28 +0200

    remove unused code

--- src/PimcoreNtrigaCookiesBundle.php
+++ src/PimcoreNtrigaCookiesBundle.php
@@ -2,9 +2,6 @@
 
 namespace Ntriga\PimcoreCookies;
 
-use Symfony\Component\Config\FileLocator;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class PimcoreNtrigaCookiesBundle extends Bundle
